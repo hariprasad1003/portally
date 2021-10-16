8f6aa6070d1412e0ea16813059e5e2178e280948
Divya-1200 <divya07msd@gmail.com>
2021-10-16 09:15:40 +0530

    event details updated in db

--- db.py
+++ db.py
@@ -6,11 +6,13 @@ import os
 from decouple import config
 from bson import ObjectId
 import random
+from datetime import date
 
 app  = Flask(__name__)
 app.config['MONGO_URI'] = config('MONGO_URI') 
 mongo = PyMongo(app)
 db_admin = mongo.db.admin_login
+db_events = mongo.db.b_events
 
 
 def generate_random():
@@ -46,6 +48,37 @@ def insert_login_details():
 
     return "Success"
 
+'''
+Events :
+	Date 		    : Calender 
+	Time 			: Time 
+	Event Name 	    : Char 
+	Event Venue     : Char 
+	Co-ordinators   : Char  
+	Form 		    : link 
+	Contact Details : link  
+	Dept 			: Char 
+	Year 			: Char
+'''
+def insert_events_students():
+   
+    today = date.today()
+    d1 = today.strftime("%d/%m/%Y")
+    
+    data = {
+        "event_id"          : 1,
+        "date"              : d1,
+        "event_name"        : "W3H Club coding event",
+        "event_venue"       : "Placement block Basil",
+        "co-ordinators"     : "Divya, Hari Prasad, Varsha",
+        "form_link"         : "https://forms.gle/9E4ofu5rqPe77cus6",
+        "contact_details"   : "9791108129",
+        "department"        : "CSE",
+        "year_of_study"     : "III"
+    }
+
+    db_events.insert_one(data)
+
 @app.route("/", methods=["GET","POST"])
 def startpy():
 
@@ -53,6 +86,9 @@ def startpy():
 
 
 if __name__ == "__main__":
+
     # print(app.config['MONGO_URI'])
     # app.run( debug = True,host="0.0.0.0",port = PORT)
-    insert_login_details()
+    # insert_login_details()
+
+    insert_events_students()
