195ab6533676bc66a138ffdbc56fa0202cc48a9e
Divya-1200 <divya07msd@gmail.com>
2021-10-17 14:36:17 +0530

    final changes

--- myapp.py
+++ myapp.py
@@ -1,9 +1,6 @@
 import dropbox
 from werkzeug.utils import secure_filename
 
-dropbox_access_token='sl.A6h_mHgkMYkojZX-kJlN9-2V0FDowb5PUYyIj3hwxM4aXySQAbNufZSrMrVCM_zbfwLG9prMLWbWKyQA_AWGCrGRPmaiH9Xl2waDcNNhtCXBlNTww1BKxJIDfuVB55IT774yGKeRJyM'
-computer_path= "app.py"
-dropbox_path= "/portally_notes/" + computer_path
 
 
 client = dropbox.Dropbox(dropbox_access_token)
@@ -14,7 +11,7 @@ client = dropbox.Dropbox(dropbox_access_token)
 with open(computer_path, 'rb') as f:
             client.files_upload(f.read(), dropbox_path)
             print("uploaded")
-            
+
 md, res = client.files_download(dropbox_path)
 data = res.content
 
